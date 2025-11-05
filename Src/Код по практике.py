ecd63b65ecd8d916f4ef4b042218f93a0c811b3c
Kiselll227 <xcvbnnccnnbb@gmail.com>
2025-11-05 15:48:53 +0300

    Add files via upload

---
+++ Src/Код по практике.py
@@ -0,0 +1,21 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
+import sys
+
+class ImageConverterUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Программа для выбора файла')
+        self.layout = QVBoxLayout()
+        self.label = QLabel('Файл не выбран')
+        self.button = QPushButton('Выбрать файл')
+        self.button.clicked.connect(self.select_file)
+        self.layout.addWidget(self.button)
+        self.file_path = None
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = ImageConverterUI()
+    window.show()
+    sys.exit(app.exec())
+
+
+
