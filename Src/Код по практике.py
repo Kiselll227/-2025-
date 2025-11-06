fbe80834abb9a57bf9efa209035725662dc126fc
Kiselll227 <xcvbnnccnnbb@gmail.com>
2025-11-06 15:25:59 +0300

    Add files via upload

--- Src/Код по практике.py
+++ Src/Код по практике.py
@@ -21,3 +21,17 @@ class ImageEditorUI(QWidget):
         if file_path:
             self.file_path = file_path
             self.labal.setText('Выбран файл: ' + file_path)
+        size_layout = QHBoxLayout()
+        size_layout.addWidget(QLabel('Новая ширина:'))
+        self.width_inpt = QLineEdit()
+        size_layout.addWidget(self.width_inpt)
+        size_layout.addWidget(QLabel('Новая высота:'))
+        self.height_inpt = QLineEdit()
+        size_layout.addWidget(self.height_inpt)
+        self.layot.addLayout(size_layout)
+
+        rotate_layout = QHBoxLayout()
+        rotate_layout.addWidget(QLabel('Поворот (градусы):'))
+        self.rotate_inpt = QLineEdit()
+        rotate_layout.addWidget(self.rotate_inpt)
+        self.layot.addLayout(rotate_layout)
