3be5c8abc2ff53a6fd3321b169af357608ed155d
Kiselll227 <xcvbnnccnnbb@gmail.com>
2025-11-06 15:21:45 +0300

    Add files via upload

--- Src/Код по практике.py
+++ Src/Код по практике.py
@@ -1,20 +1,23 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
+from PyQt6.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QPushButton, QLabel,
+    QFileDialog, QLineEdit, QHBoxLayout
+)
+from PIL import Image
 import sys
-class ImageConverterUI(QWidget):
+class ImageEditorUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Программа для выбора файла')
-        self.layout = QVBoxLayout()
-        self.label = QLabel('Файл не выбран')
-        self.button = QPushButton('Выбрать файл')
+        self.setWindowTitle('Редактор изображений')
+        self.layot = QVBoxLayout()
+        self.labal = QLabel('Файл не выбран')
+        self.layot.addWidget(self.labal)
+        self.button = QPushButton('Выбрать изображение')
         self.button.clicked.connect(self.select_file)
-        self.layout.addWidget(self.button)
+        self.layot.addWidget(self.button)
+        self.setLayout(self.layot)
         self.file_path = None
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = ImageConverterUI()
-    window.show()
-    sys.exit(app.exec())
-
-
-
+    def select_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Выберите изображение', '', "Images (*.png *.jpg)")
+        if file_path:
+            self.file_path = file_path
+            self.labal.setText('Выбран файл: ' + file_path)
